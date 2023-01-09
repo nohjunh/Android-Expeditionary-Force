67913d9c6ef91cc95151ec17d6cc10b4642e3011
nohjunh <nsh042777@naver.com>
2023-01-09 18:56:17 +0900

    [RV] sampleApp init setting

---
+++ RV/sampleApp/app/src/main/java/com/nohjunh/sampleapp/flowerList/FlowersListActivity.kt
@@ -0,0 +1,12 @@
+package com.nohjunh.sampleapp.flowerList
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nohjunh.sampleapp.R
+
+class FlowersListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flowers_list)
+    }
+}
